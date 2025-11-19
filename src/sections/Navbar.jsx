74b0070239da1c8097f456dcb70b76735fa43f27
Aneerudh17 <paneerudh@gmail.com>
2025-11-19 14:10:41 +0530

    updated animations

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { socials } from '../constants';
 import { useGSAP } from '@gsap/react';
 import gsap from "gsap";
@@ -11,6 +11,8 @@ const Navbar = () => {
     const bottomLineRef = useRef(null);
     const tl = useRef(null);
     const [isOpen, setIsOpen] = useState(false);
+    const iconTl = useRef(null);
+    const [showBurger, setShowBurger] = useState(true);
 
     useGSAP(()=>{
         gsap.set(navRef.current,{ xPercent:100 })
@@ -39,14 +41,41 @@ const Navbar = () => {
     },
     "<+0.2"
     );
-    });
+    iconTl.current = gsap.timeline({paused:true}).to(topLineRef.current,{
+        rotate:45,
+        y:3.3,
+        duration:0.3,
+        ease:"power2.inOut"}).to(bottomLineRef.current,{
+            rotate:-45,
+            y:-3.3,
+            duration:0.3,
+            ease:"power2.inOut"
+        },"<");
+    },[]);
+
+    useEffect(()=>{
+        let lastScrolly = window.scrollY;
+        const handleScroll = () => {
+            const currentScrolly = window.scrollY;
+
+            setShowBurger(currentScrolly <= lastScrolly || currentScrolly < 10);
+
+            lastScrolly = currentScrolly;
+        };
+        window.addEventListener("scroll",handleScroll,{
+            passive: true,
+        });
+        return ()=> window.removeEventListener("scroll",handleScroll);
+    },[]);
 
     const toggleMenu = ()=> {
         if(isOpen){
-            tl.current.reverse()
+            tl.current.reverse();
+            iconTl.current.reverse();
         }
         else{
             tl.current.play();
+            iconTl.current.play();
         }
         setIsOpen(!isOpen);
     };
@@ -94,7 +123,13 @@ const Navbar = () => {
     <div className='fixed z-50 flex flex-col items-center
     justify-center gap-1 transition-all duration-300 bg-black
     rounded-full cursor-pointer w-14 h-14 md:w-20 md:h-20
-    top-4 right-10' onClick={toggleMenu}>
+    top-4 right-10' 
+    onClick={toggleMenu}
+    style={
+          showBurger
+            ? { clipPath: "circle(50% at 50% 50%)" }
+            : { clipPath: "circle(0% at 50% 50%)" }
+        }>
         <span
         ref={topLineRef} 
         className='block w-8 h-0.5 bg-white rounded-full origin-center'></span>
