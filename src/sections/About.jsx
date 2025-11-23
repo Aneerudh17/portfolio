fb094875acf20f3e480c92d3453b7b1f5097ecea
Aneerudh17 <paneerudh@gmail.com>
2025-11-23 19:09:02 +0530

    updated about

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -48,7 +48,7 @@ Practice Problem Solving.`;
       <div className="flex flex-col items-center justify-between gap-16 px-10 pb-16 text-xl font-light tracking-wide lg:flex-row md:text-2xl lg:text-3xl text-white/60">
         <img
           ref={imgRef}
-          src="images/man.jpg"
+          src="images/Mac.png"
           alt="man"
           className="w-md rounded-3xl"
         />
